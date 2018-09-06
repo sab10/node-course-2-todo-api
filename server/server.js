724adefc04a8b37f6478b568a899f2d0e5792043
Sabin <sabin@localhost.localdomain>
2018-09-06 18:39:12 +0200

    Add get /todos route

--- server/server.js
+++ server/server.js
@@ -47,6 +47,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log('Started to listen on port ',port);
 });
