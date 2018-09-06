e68db2fb234597983b402614f4fb13476cd0f77a
Sabin <sabin@localhost.localdomain>
2018-09-06 18:56:29 +0200

    Add test for get /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,8 +6,17 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
 
+const todos = [{
+  text : 'First test to do'
+}, {
+  text : 'Second test to do'
+}];
+
 beforeEach((done) => { // the beforeEach methos executes automatically before every test case, and the test case will not be executed if the before each not work properly
   Todo.deleteMany().then(() => { // this method cancel all the data in the collection before to add the new one, because in the next lines we check the first element of the collection and we want it to be the one we insert now and there is the check for todos.length to be 1 so only one alement can be in the table to pass the test
+    //done(); // before, when we was testing only post in this method was only this command
+    return Todo.insertMany(todos);
+  }).then(() => {
     done();
   });
 });
@@ -27,7 +36,7 @@ describe('POST /todos', () => {
       if(err) {
         return done(err);
       }
-      Todo.find().then((todos) => {
+      Todo.find({text}).then((todos) => {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -47,7 +56,7 @@ describe('POST /todos', () => {
         return done(err);
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => {
         done(e);
@@ -61,3 +70,16 @@ describe('POST /todos', () => {
 // i solved the first one and was an error writing code
 // the second is Uncaught Error: listen EADDRINUSE :::3000
 // I SOLVED THE PROBLEM ABOVE, so the problem was that the port was already used because I was leaving the server opened, I needed to close it before to launche the test!!!!!!!!!!!!!!!!!!
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  })
+});
