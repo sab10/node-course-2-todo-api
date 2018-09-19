65d9e6b18957840abb4934cab5fa093f8534ad86
Sabin <sabin@localhost.localdomain>
2018-09-19 10:29:09 +0200

    Associate todos with users

--- server/server.js
+++ server/server.js
@@ -40,12 +40,13 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
 //  console.log(req.body);// this stamps the post request that is coming from the browser
 //  res.send('nothing special'); // this give the response to the server
 
   var todo = new Todo({
-    text : req.body.text
+    text : req.body.text,
+    _creator : req.user._id
   });
 
   todo.save().then((doc) => {
@@ -55,38 +56,46 @@ app.post('/todos', (req, res) => {
   }) ;
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator : req.user.id
+  }).then((todos) => {
     res.send({todos});
   }).catch((e) => {
     res.status(400).send(e);
   }) ;
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)){
     res.status(404).send();
   }
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id : id,
+    _creator : req.user._id
+  }).then((todo) => {
     if(todo){
       res.send({todo});
     }
-    res.status(404).send();
+    res.status(404).send('No todos to show with your permission');
   }).catch((e) => {
     res.status(400).send(e);
   }) ;
   //res.send(req.params);  // this command send back the id wrote in the link
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id))
     res.status(404).send('Id not valid');
 
-  Todo.findByIdAndDelete(id).then((todo) => {
+  Todo.findOneAndDelete({
+    _id : id,
+    _creator : req.user._id
+  }).then((todo) => {
     if(!todo)
       res.status(400).send('This is not an error, there is not a doc with that ID');
 
@@ -96,7 +105,7 @@ app.delete('/todos/:id', (req, res) => {
   }) ;
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -110,7 +119,10 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((todo) => {
+  Todo.findOneAndUpdate({
+    _id : id,
+    _creator : req.user._id
+  }, {$set : body}, {new : true}).then((todo) => {
     if(!todo)
       res.status(404).send('nessun riscontro');
 
